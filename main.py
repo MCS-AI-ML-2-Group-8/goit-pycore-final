f370662074da9c10c3ec673bab66086443ac86a6
Andrii Horbokon <andrii.horbokon@gmail.com>
2025-11-11 20:21:02 +0200

    Keep uvicorn only

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from api.endpoints import app
 from cli.main_loop import launch_main_loop
 
 def main():
-    if "--api" in sys.argv:
-        uvicorn.run(app, host="0.0.0.0", port=8000)
-    else:
-        launch_main_loop()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 main()
