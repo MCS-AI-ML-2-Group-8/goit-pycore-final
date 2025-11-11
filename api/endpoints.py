1f98156c35ecc6e0686f8a785c009e5b3dd185aa
Andrii Horbokon <andrii.horbokon@gmail.com>
2025-11-11 20:24:54 +0200

    Health check

--- api/endpoints.py
+++ api/endpoints.py
@@ -3,3 +3,7 @@ from api.contact_endpoints import router as contacts_router
 
 app = FastAPI()
 app.include_router(contacts_router)
+
+@app.get("/")
+def health_check() -> str:
+    return "Live"
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from api.endpoints import app
 from cli.main_loop import launch_main_loop
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
 
 main()
