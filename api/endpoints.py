d91ca8bdf6f775185dea55ae3d2322b90c8ec069
msselizabeth <lizagotovitryby@gmail.com>
2025-11-11 19:28:22 -0500

    add notes router

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from api.contact_endpoints import router as contacts_router
+from api.notes_endpoints import router as notes_router
 
 app = FastAPI()
 app.include_router(contacts_router)
+app.include_router(notes_router)
+
