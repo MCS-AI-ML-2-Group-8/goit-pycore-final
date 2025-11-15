5d5a93527c294cf9dcf27d92998782d20425e484
Andrii Horbokon <andrii.horbokon@gmail.com>
2025-11-15 14:14:22 +0200

    Setup database path

--- data/database.py
+++ data/database.py
@@ -1,8 +1,12 @@
+import os
 from pathlib import Path
 from sqlalchemy import create_engine
 from data.models import Base
 
-database_path = Path.home() / "contacts.db"
+configured_path = os.getenv("Magic_DB_PATH")
+configured_name = "contacts.db"
+
+database_path = Path(configured_path) / configured_name if configured_path else Path.home() / configured_name
 database_engine = create_engine(f"sqlite:///{database_path.resolve()}")
 
 Base.metadata.create_all(database_engine)
