ff717f550bd4d7e84e2cb7b7ee2e5046457ebfdc
Andrii Horbokon <andrii.horbokon@gmail.com>
2025-11-15 19:44:35 +0200

    Remove unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,6 @@ import { Chat } from "./components/Chat/Chat";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  // Use this snippet to verify CORS settings in localhost
-
-  // const fetchContacts = async() => {
-  //   const response = await fetch("http://localhost:8000/contacts");
-  //   const contacts = await response.json()
-  //   console.info(contacts)
-  // }
-
-  // useEffect(() => { fetchContacts() }, [])
-
   return (
     <div className="flex flex-col items-center justify-center p-4">
       <Chat />
