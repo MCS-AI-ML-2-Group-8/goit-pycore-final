9f9bb9a4168b91d07a87c05d1386c36a5601700e
msselizabeth <lizagotovitryby@gmail.com>
2025-11-14 20:09:04 -0500

    update APP + css to tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { Chat } from "./components/Chat/Chat";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="flex flex-col justify-center gap-3 max max-w-7xl m-auto">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    
-    </>
-  )
+    <div className="flex flex-col items-center justify-center p-4">
+      <Chat />
+    </div>
+  );
 }
 
-export default App
+export default App;
